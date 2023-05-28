710c75a056c33f166057d591d8422a82f3dcb121
emandayrit <adridayrit@gmail.com>
2023-05-28 07:24:02 -0800

    Added Game Manager Script

--- Assets/Code/Scripts/GameManager.cs
+++ Assets/Code/Scripts/GameManager.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int gameScore;
+    public bool isGameOver;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
