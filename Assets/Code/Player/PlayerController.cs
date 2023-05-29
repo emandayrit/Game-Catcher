9f7165e61874f4796ec6c6543920ae4008d2efb0
emandayrit <adridayrit@gmail.com>
2023-05-29 03:04:12 -0800

    Created Player Controller

--- Assets/Code/Player/PlayerController.cs
+++ Assets/Code/Player/PlayerController.cs
@@ -1,18 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] float xSpeed, xInput;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        xInput = Input.GetAxis("Horizontal");
+    }
+
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().velocity = new Vector3(xInput * xSpeed, 0, 0) * Time.deltaTime;
     }
 }
