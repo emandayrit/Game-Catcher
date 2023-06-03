1a906ded64ef11338a681cc873f63026806c27e5
Ronin <kkrondy@gmail.com>
2023-06-04 04:11:57 +0800

    Score Display Update

--- Assets/Code/Scripts Managers/ScoreManager.cs
+++ Assets/Code/Scripts Managers/ScoreManager.cs
@@ -1,13 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
     [SerializeField] int score;
+    [SerializeField] Text scoreText;
+
+    private void Start()
+    {
+        score = 0;
+        scoreText.text = "Score: " + score.ToString();
+    }
 
     public void ScoreCall()
     {
         Debug.Log($"Your score: {++score}");
+        scoreText.text = "Score: " + score.ToString();
     }
 }
