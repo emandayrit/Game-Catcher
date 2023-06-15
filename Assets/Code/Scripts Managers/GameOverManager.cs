8f4fbbeb4f5049b20aeeec810dd5ab2ea5cf5da9
emandayrit <adridayrit@gmail.com>
2023-06-14 20:43:51 -0800

    Pls Revert 2.0

--- Assets/Code/Scripts Managers/GameOverManager.cs
+++ Assets/Code/Scripts Managers/GameOverManager.cs
@@ -1,18 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GameOverManager : MonoBehaviour
 {
     public bool IsGameOver;
 
-    [SerializeField] SpawnManager spawnManager;
-
     public void GameOverCall()
     {
         Debug.Log($"Game Over. You cannot move and coins will not spawn anymore.");
         IsGameOver = true;
-
-        spawnManager.enabled = false;
     }
 }
