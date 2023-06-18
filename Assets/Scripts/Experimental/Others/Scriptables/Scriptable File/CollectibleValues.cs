944ba92d365e6b48fe9606a0bfc65f7af36d0f78
emandayrit <adridayrit@gmail.com>
2023-06-18 01:27:15 -0800

    Added Audio (SFX and BGM)

--- Assets/Scripts/Experimental/Others/Scriptables/Scriptable File/CollectibleValues.cs
+++ Assets/Scripts/Experimental/Others/Scriptables/Scriptable File/CollectibleValues.cs
@@ -6,11 +6,20 @@ public class CollectibleValues : ScriptableObject
     public int value;
     public float vfxTimer; 
     [SerializeField] GameObject vfx;
+    [SerializeField] AudioClip clip;
 
     public void PlayVFX(Transform parent)
     {
         GameObject _vfx = Instantiate(vfx,parent);
+        PlaySFX();
         _vfx.GetComponent<ParticleSystem>()?.Play();
         Destroy(_vfx,vfxTimer);
+    }
+
+    void PlaySFX()
+    {
+        AudioSource source = GameObject.Find("SFX").GetComponent<AudioSource>();
+        source.clip = clip;
+        source.Play();
     }
 }
